9ea0cb7de4703a18d9aa7a5e0ebdd501cb79ccfa
jinbekim <59330110+jinbekim@users.noreply.github.com>
2022-01-14 22:50:41 +0900

    Time: 134 ms (26.46%), Space: 40.1 MB (92.73%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.js
@@ -5,24 +5,13 @@
 var myAtoi = function(s) {
     const Max = 2147483647;
     const Min = -2147483648;
-    let sign = 1;
-    let idx = 0;
-    let num = 0;
-    
-    while (s[idx] == ' ') idx++;
-    if (s[idx] === '+' || s[idx] === '-'){
-        if (s[idx] === '-') sign = -1;
-        idx++;
-    }
-    while (48 <= s.charCodeAt(idx) && s.charCodeAt(idx) <= 57 )
-    {
-        num = num * 10 + s[idx] * sign;
+    const num = parseInt(s);
         if (num > Max)
             return Max;
         if (num < Min)
             return Min;
-        idx++;
-    }
-    
+        if (isNaN(num))
+            return 0;
+
     return num
 };
