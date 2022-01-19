286f5df653a2a2e576b727eabb47ca04dd7c24bb
jinbekim <59330110+jinbekim@users.noreply.github.com>
2022-01-19 23:20:27 +0900

    Time: 92 ms (88.98%), Space: 41.1 MB (59.83%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    let zeroArr = new Array();
+    
+    matrix.forEach((val, row) => {
+        val.forEach((val, col) => {
+            if (val == 0) zeroArr.push([row, col]);
+        });
+    });
+    for (const element of zeroArr) {
+        matrix.forEach((val, idx, arr) => {
+            val[element[1]] = 0
+            if (idx === element[0]) {
+                for (let i = 0,len = matrix[idx].length; i < len; ++i )
+                    matrix[idx][i] = 0;
+            }
+        });
+    }
+    return zeroArr;
+};
