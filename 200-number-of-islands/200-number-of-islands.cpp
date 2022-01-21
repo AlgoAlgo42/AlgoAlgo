27469838b33ba46e36398acfb8bc44592d4105d6
DevSushin <72022736+sushin-42@users.noreply.github.com>
2022-01-21 16:50:00 +0900

    Time: 47 ms (42.59%), Space: 12.3 MB (59.03%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
-    int numIslands(vector<vector<char>>& grid) {
-        int islands = 0;
+void visitAdjLand(vector<vector<char>> &grid, int i, int j, int rows, int cols)
+{
+    if (i >= 0 && j >= 0 && i < rows && j < cols && grid[i][j] != '2' && grid[i][j] != '0')
+    {
+        grid[i][j] = '2';
+        visitAdjLand(grid, i + 1, j, rows, cols);
+        visitAdjLand(grid, i, j + 1, rows, cols);
+        visitAdjLand(grid, i - 1, j, rows, cols);
+        visitAdjLand(grid, i, j - 1, rows, cols);
+    }
+
+    return;
+}
+int numIslands(vector<vector<char>> &grid)
+{
+    int islands = 0;
     int rows = grid.size(), cols = grid[0].size();
     for (int i = 0; i < rows; i++)
     {
@@ -10,28 +24,11 @@ public:
             if (grid[i][j] == '1')
             { 
                 visitAdjLand(grid, i, j, rows, cols);
+              
                 islands++;
             }
         }
     }
     return islands;
-    }
-    void visitAdjLand(vector<vector<char>> &grid, int i, int j, int rows, int cols)
-{
-    if (i < 0 || j < 0 || i >= rows || j >= cols)
-    {
-        return;
-    }
-    else if (grid[i][j] == '2' || grid[i][j] == '0')
-    { 
-        return;
-    }
-    grid[i][j] = '2'; 
-    visitAdjLand(grid, i+1, j, rows, cols);
-    visitAdjLand(grid, i, j+1, rows, cols); 
-    visitAdjLand(grid, i-1, j, rows, cols);
-    visitAdjLand(grid, i, j-1, rows, cols); 
-
-    return;
 }
 };
