9fc79eff6b823ff84b7878e384f4377b2869945f
jinbekim <59330110+jinbekim@users.noreply.github.com>
2022-01-23 12:11:01 +0900

    Time: 94 ms (31.25%), Space: 38.2 MB (93.75%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.js
+++ 1291-sequential-digits/1291-sequential-digits.js
@@ -9,13 +9,8 @@ var sequentialDigits = function (low, high) {
 	const base = '123456789';
 	const result = [];
 
-	const lowToString = String(low);
-	const highToString = String(high);
-	const lowDigit = lowToString.length;
-	const highDigit = highToString.length;
-
-	for (let j = lowDigit; j <= highDigit; ++j) {
-		for (let i = 0; i <= 9 - j; ++i) {
+	for (let j = 0; j <= 9; ++j) {
+		for (let i = 0; i <= 9-j; ++i) {
 			const num = Number(base.slice(i, i + j));
 			if (num >= low && num <= high) result.push(num);
 		}
