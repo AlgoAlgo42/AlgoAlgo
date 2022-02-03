1ffd5acf39cb2c22616cf2c0a5fe9d41270b2040
jinbekim <jinbekim@student.42seoul.kr>
2022-02-03 21:56:55 +0900

    lv.1 숫자 문자열과 영단어

---
+++ 숫자문자열과영단어.js
@@ -0,0 +1,13 @@
+function solution(s) {
+	return s
+		.replace(/one/g, '1')
+		.replace(/two/g, '2')
+		.replace(/three/g, '3')
+		.replace(/four/g, '4')
+		.replace(/five/g, '5')
+		.replace(/six/g, '6')
+		.replace(/seven/g, '7')
+		.replace(/eight/g, '8')
+		.replace(/nine/g, '9')
+		.replace(/zero/g, '0');
+}
