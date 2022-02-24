46fe4c53f1004327eb4c3d90373c6e59343c41bf
hyopark <hyopark@hyoparkui-MacBookAir.local>
2022-02-24 23:52:01 +0900

    소수찾기

--- 멀쩡한-사각형/fine-square.cpp
+++
@@ -1,8 +0,0 @@
-using namespace std;
-#include <iostream>
-long long solution(int w,int h) {
-    long long s = 0;
-    for (double i = 0.0; i  < w ; i+=1.0)
-        s += (int)(i * (double)h / (double)w);
-    return (2*s);
-}
