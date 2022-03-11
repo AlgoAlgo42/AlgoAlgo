a406b7bb8cb59823bc5a43122ec4e018eedf0c74
Chang hyun Jang <cjang@student.42seoul.kr>
2022-03-11 16:32:13 +0900

    단어공부 fin cjang

--- 단어공부/cjang.cpp
+++ 단어공부/cjang.cpp
@@ -8,28 +8,25 @@ int	main(void)
 {
 	vector<int>	v(27);
 	string		s;
-	vector<int>	max(3);	// max값, max값인 갯수, max값인 알파벳
+	vector<int>	max(2);	// max값, max값인 알파벳
 
 	cin >> s;
 	for (char c : s)
 		++v[c % 32];
+
 	for (int i = 1; i < 27; i++)
 	{
 		if (max[0] < v[i])
 		{
 			max[0] = v[i];
-			max[1] = 1;
-			max[2] = i + 64;
+			max[1] = i + 64;
 		}
 		else if (max[0] == v[i])
-		{
-			++max[1];
-			max[2] = '?';
-		}
+			max[1] = '?';
 	}
-	cout << static_cast<char>(max[2]);
+
+	cout << static_cast<char>(max[1]);
 }
 // 			a = 97 / z = 122 / A = 65 / Z = 90
 // % 32->	a = 1 / z = 26 / A = 1 / Z = 26
 // 출력시 -> +64
-
