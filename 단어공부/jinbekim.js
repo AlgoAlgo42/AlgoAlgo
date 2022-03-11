907fa9446d1504d8b1e6494c09f066e20a9682dd
jinbekim <jinbekim@student.42seoul.kr>
2022-03-11 17:08:35 +0900

    공백문자 제거

--- 단어공부/jinbekim.js
+++ 단어공부/jinbekim.js
@@ -1,18 +1,23 @@
 //한 줄 입력
 
 let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString();
+let input = fs
+	.readFileSync('/dev/stdin')
+	.toString()
+	.trim();
 
 const str = input.toUpperCase().split('');
 const dic = {};
 const max = {
-	key: 'A',
+	key: '?',
 	count: 0
 };
 
 for (key of str) {
+	if (key === '\n') break;
 	dic[key] = dic[key] ? dic[key] + 1 : 1;
 }
+
 for (key in dic) {
 	if (max.count < dic[key]) {
 		max.key = key;
